d85da971177c1573aebb2896c9f598365155e100
Renan Domingos <renan.domingos1@gmail.com>
2014-11-17 21:13:56 -0200

    Integração das fotos #15

--- application/views/home_content_usuario.php
+++ application/views/home_content_usuario.php
@@ -1,4 +1,32 @@
 <!-- Content -->
+<link href="/upload/uploadify.css" media="screen" rel="stylesheet"/>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+<script src="/upload/jquery.uploadify-3.1.min.js"></script>
+<script>
+    $(document).ready(function () {
+ 
+        var base_url = '<?php echo base_url(); ?>';
+ 
+        $('#upload-file').click(function (e) {
+            e.preventDefault();
+            $('#userfile').uploadify('upload', '*');
+        });
+ 
+        $('#userfile').uploadify({
+            'auto':false,
+            'swf': base_url + 'assets/js/jquery/uploadify_31/uploadify.swf',
+            'uploader': base_url + 'uploadify_v3/do_upload',
+            'cancelImg': base_url + 'assets/javascript/jquery/uploadify_31/uploadify-cancel.png',
+            'fileTypeExts':'*.jpg;*.bmp;*.png;*.tif',
+            'fileTypeDesc':'Image Files (.jpg,.bmp,.png,.tif)',
+            'fileSizeLimit':'2MB',
+            'fileObjName':'userfile',
+            'buttonText':'Select Photo(s)',
+            'multi':true,
+            'removeCompleted':false
+        });
+    });
+</script>
 <div id="content">
     <div class="inner">
 
